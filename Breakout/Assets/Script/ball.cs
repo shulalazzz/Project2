beb65e281fc4b325ebf16bfce722855f9675fc41
shulalazzz <zj2060203064@gmail.com>
2020-03-29 14:58:10 -0500

    Add project with basic features

---
+++ Breakout/Assets/Script/ball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    private Rigidbody rb_ball;
+    private float radius;
+    public float speed;
+    public Transform paddle;
+    void Start()
+    {
+        rb_ball = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)&&!game_manage.instance.isPlaying)
+        {
+           game_manage.instance.isPlaying = true;
+           Vector3 speed_normalized =  new Vector3(1f, 1f, 0).normalized;
+           rb_ball.velocity = speed_normalized * speed;
+        }
+        if (game_manage.instance.isPlaying == false)
+        {
+            radius = gameObject.GetComponent<SphereCollider>().radius;
+            Vector3 pos = transform.position;
+            pos.x = paddle.transform.position.x;
+            pos.y = paddle.transform.position.y + radius;
+            transform.position = pos;
+        }
+    }
+}
---
+++ Breakout/Assets/Script/game_manage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game_manage : MonoBehaviour
+{
+    public static game_manage instance;
+    public bool isPlaying = false;
+     void Awake()
+    {
+        instance = this;
+    }
+    public void Reset_ball()
+    {
+        isPlaying = false;
+    }
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
