fc704dc9291971963990988d0818020571ab8765
shulalazzz <zj2060203064@gmail.com>
2020-04-05 17:02:47 -0500

    Added a menu and an empty multiplayer mode

---
+++ Breakout/Assets/Script/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void PlaySingle()
+    {
+        SceneManager.LoadScene("single_mode");
+    }
+    public void PlayMulti()
+    {
+        SceneManager.LoadScene("multi_mode");
+    }
+    public void Quit()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+}
