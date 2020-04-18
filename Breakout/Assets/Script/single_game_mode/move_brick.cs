edf326c6da10f73ba63eb9f6e48cbed6f19e23fa
Feiran Zhang <stevenzfr@ku.edu>
2020-04-18 17:51:36 -0500

    add move_brick scripts and prefab
    unbreakable and breakable in two differen color

---
+++ Breakout/Assets/Script/single_game_mode/move_brick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class move_brick : MonoBehaviour
+{
+    public float speed;
+    public float start;
+    public float end;
+    private Transform son;
+    public bool moveToLeft = true;
+    private void Start()
+    {
+        son = this.transform;
+    }
+    private void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        if (son.position.x <= end && moveToLeft)
+        {
+            moveToLeft = false;
+        }
+        else if (son.position.x >= start && !moveToLeft)
+            moveToLeft = true;
+        son.position += (moveToLeft ? Vector3.left : Vector3.right) * Time.deltaTime * speed;
+    }
+}
